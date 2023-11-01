c1bad8202b67e8df2415cc7d7a8399bcf31f54cf
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2023-11-01 17:54:52 +0200

    feat: create component

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       colors: {
         "light-red": "hsl(0, 100%, 67%)",
         "orangey-yellow": "hsl(39, 100%, 56%)",
-        "green-teal": "hsl(39, 100%, 56%)",
+        "green-teal": "hsl(166, 100%, 37%)",
         "cobalt-blue": "hsl(234, 85%, 45%)",
         "light-slate-blue": "hsl(252, 100%, 67%)",
         "light-royal-blue": "hsl(241, 81%, 54%)",
